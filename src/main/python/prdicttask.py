baff9c5b78c4f5a5a168814672545eb0839b1f2f
Matt Jacobs <mattrjacobs@gmail.com>
2011-09-09 23:30:10 -0600

    Removing bucketed comps for task computation

--- src/main/python/prdicttask.py
+++ src/main/python/prdicttask.py
@@ -3,12 +3,6 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 from handlers.task.eventtiming import EventTimingTaskHandler
 
-import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-
-#from google.appengine.dist import use_library
-#use_library('django', '1.2')
-
 urlmap = [('/task/events/timing', EventTimingTaskHandler)]
 application = webapp.WSGIApplication(urlmap, debug=True)
 
