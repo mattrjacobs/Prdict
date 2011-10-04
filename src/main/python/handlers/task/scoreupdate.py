746c23ded34def9d022a8cd7cd6d2991b2dc13b2
Matt Jacobs <mattrjacobs@gmail.com>
2011-10-03 20:10:47 -0600

    GH-118, GH-119: Fanfeedr interaction is better + Using memcache to prevent more than 25 reqs per hour + Adding a sleep to Fanfeedr call to make sure we don't exceed QPS

--- src/main/python/handlers/task/scoreupdate.py
+++ src/main/python/handlers/task/scoreupdate.py
@@ -3,6 +3,7 @@ from handlers.handler import AbstractHandler
 import datetime
 import logging
 import simplejson as json
+import time
 
 from google.appengine.ext import db
 
@@ -26,16 +27,12 @@ class ScoreUpdateTaskHandler(AbstractHandler):
         logging.error("Shouldn't be receiving a POST here")
 
     def handle_score_update(self):
-        logging.info("Got request to do a score update")
         query = db.GqlQuery("SELECT * FROM SportsEvent WHERE start_date < :1 AND completed = false ORDER BY start_date ASC", datetime.datetime.utcnow())
         current_games = query.fetch(25, 0)
-        logging.info("NUM CURRENT : %d" % len(current_games))
-        logging.info("For each game, update it's score and potentially close it out")
         for game in current_games:
             if self.score_svc.can_get_score(game):
-                logging.info("GAME : %s" % game.title)
-                logging.info("GAME START : %s" % game.nice_start_date_est)
-                logging.info("REF ID : %s" % game.ref_id)
+                #this prevents us from exceeding Fanfeedr QPS limit
+                time.sleep(1)
                 score = self.score_svc.get_score(game)
                 score["type"] = "sportsevent"
                 score["ref_id"] = game.ref_id
--- src/main/python/services/score_svc.py
+++ src/main/python/services/score_svc.py
@@ -2,50 +2,44 @@ import datetime
 import logging
 import simplejson as json
 
+from google.appengine.api import memcache
 from google.appengine.api import urlfetch
 
 class ScoreService:
     API_KEY = "hqxv2tyfmmhu68zfn9nfsfsv"
     MAX_REQS_PER_HOUR = 25
-
-    def __init__(self):
-        self.current_hour = datetime.datetime.utcnow().hour
-        self.reqs_this_hour = 0
+    CURRENT_HOUR_KEY = "score_current_hour"
+    REQS_THIS_HOUR_KEY = "scores_this_hour"
 
     def can_get_score(self, event):
+        current_hour = memcache.get(ScoreService.CURRENT_HOUR_KEY)
+        if not current_hour:
+            current_hour = datetime.datetime.utcnow().hour
+            memcache.set(ScoreService.CURRENT_HOUR_KEY, current_hour)
+        reqs_this_hour = memcache.get(ScoreService.REQS_THIS_HOUR_KEY)
+        if not reqs_this_hour:
+            reqs_this_hour = 0
         hour_at_req = datetime.datetime.utcnow().hour
-        logging.info("HOUR CHECKED : %s" % hour_at_req)
-        logging.info("HOUR AT START : %s" % self.current_hour)
-        if hour_at_req == self.current_hour:
-            logging.info("MAX ALLOWED : %d" % ScoreService.MAX_REQS_PER_HOUR)
-            logging.info("NUM USED : %d" % self.reqs_this_hour)
-            if self.reqs_this_hour < ScoreService.MAX_REQS_PER_HOUR:
-                logging.info("Allowing this request, since not at limit yet")
-                self.reqs_this_hour = self.reqs_this_hour + 1
+        if hour_at_req == current_hour:
+            if reqs_this_hour < ScoreService.MAX_REQS_PER_HOUR:
+                memcache.set(ScoreService.REQS_THIS_HOUR_KEY, reqs_this_hour + 1)
                 return True
             else:
-                logging.info("Already at limit for hour")
                 return False
         else:
-            logging.info("Allowing this request, since we're in new hour")
-            self.current_hour = hour_at_req
-            self.reqs_this_hour = 1
+            memcache.set(ScoreService.CURRENT_HOUR_KEY, hour_at_req)
+            memcache.set(ScoreService.REQS_THIS_HOUR_KEY, 1)
             return True
 
     def get_score(self, event):
-        logging.info("Getting live score for : %s" % event.title)
+        logging.debug("Getting live score for : %s" % event.title)
         url = "http://ffapi.fanfeedr.com/basic/api/leagues/%s/events/%s?api_key=%s" % \
               (event.league.ref_id, event.ref_id, ScoreService.API_KEY)
         resp = urlfetch.fetch(url, deadline = 10)
         if resp.status_code == 200:
             json_resp = json.loads(resp.content)
-            logging.info("STATUS : %s" % json_resp["status"])
-            logging.info("LOWER : %s" % json_resp["status"].lower())
-            logging.info("COMPLETED : %s" % str(json_resp["status"].lower() == "final"))
             return { 'home_team_score' : json_resp["home_team"]["score"],
                      'away_team_score' : json_resp["away_team"]["score"],
                      'completed' : str(json_resp["status"].lower() == "final") }
         else:
-            logging.info("BAD STATUS : %d" % resp.status_code)
-            logging.info("BAD RESP : %s" % resp.content)
             return { }
