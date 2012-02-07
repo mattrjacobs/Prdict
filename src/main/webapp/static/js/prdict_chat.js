895b4a7736f500069bb8a6a290809d0c0adea4c4
Matt Jacobs <mattrjacobs@gmail.com>
2012-02-07 06:41:28 -0700

    GH-105: Removed old chat code

--- src/main/webapp/static/js/prdict_chat.js
+++ src/main/webapp/static/js/prdict_chat.js
@@ -235,34 +235,3 @@ $(function(){
     window.App = new AppView({model: {'chatMessages' : new PaginatedChatMessagesWrapper,
                                       'chatSubmit'   : new ChatSubmit }});
 });
-
-/*jQuery(document).ready(function(){
-    jQuery('input.newMsgContent').focus();
-
-    jQuery('.newChatForm').submit( function() {
-        $.ajax({
-            url     : $(this).attr('action'),
-            type    : $(this).attr('method'),
-            dataType: 'html',
-            data    : $(this).serialize(),
-            error   : function() {
-                console.debug("Failure sending new chat message");
-            },
-            success : function() {
-                jQuery('input.newMsgContent').attr('value', '');
-                jQuery('input.newMsgContent').focus();
-                console.debug("Success sending chat message");
-            }
-        });
-        return false;
-    });
-
-    jQuery('.spinner')
-        .hide()  // hide it initially
-        .ajaxStart(function() {
-            $(this).show();
-        })
-        .ajaxStop(function() {
-            $(this).hide();
-        });
-        });*/
